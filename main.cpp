df18f80bf1ae9fc55fa4256a9420613d92a5f593
LeonardoFornaro1 <91632279+LeonardoFornaro1@users.noreply.github.com>
2021-11-14 18:34:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main() {
+   int z;
+    cin >> z;
+    int primo=0;
+    int secondo=1;
+    int terzo=1;
+
+    if(z>=1){
+    while(terzo<=z){
+        cout << terzo << endl;
+        terzo=primo+secondo;
+        primo=secondo;
+        secondo=terzo;
+    }
+    }
+    else{
+        cout << "errore" << endl;
+    }
    return 0;
 }
